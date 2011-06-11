4ce9ed759e2364888e83acd718ea574ccb43321d
Nightgunner5 (Ben L.) <nightgunner5@llamaslayers.net>
2011-06-11 14:45:21 -0500

    I finally got SRCDS installed. Now onto real testing...

--- pubcomp.js
+++ pubcomp.js
@@ -1,13 +1,16 @@
-var http = require('http'), 
+var http = require('http'),
+	io = require('socket.io'),
+	fs = require('fs'),
 	spawn = require('child_process').spawn,
-	socket = require('net').Socket,
+	Sock = require('net').Socket,
 	parse = require('url').parse,
 	tf2 = null,
 	tf2_rcon = '',
 	rcon_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split( '' ),
 	rcon = null,
 	buffer = '',
-	ip = '127.0.0.1';
+	ip = 'nightgunner5.is-a-geek.net',
+	rconbuffer = '';
 
 function rconInt( num ) {
 	return String.fromCharCode( num % 256, ( num >> 8 ) % 256, ( num >> 16 ) % 256, ( num >> 24 ) % 256 );
@@ -16,7 +19,15 @@ function rconInt( num ) {
 // Status 3 is normal, status 2 means password.
 function rconSend( status, command ) {
 	var packet = '\0\0\0\0' + rconInt( status ) + command + '\0\0';
-	rcon.write( rconInt( packet.length ) + packet );
+	if ( rcon ) {
+		rcon.write( rconInt( packet.length ) + packet );
+		if ( rconbuffer ) {
+			rcon.write( rconbuffer );
+			rconbuffer = '';
+		}
+	} else {
+		rconbuffer += rconInt( packet.length ) + packet;
+	}
 }
 
 function spawnTF2( mode ) {
@@ -36,53 +47,84 @@ function spawnTF2( mode ) {
 			break;
 	}
 
+	buffer = '';
+
 	// Start TF2 server
-	tf2 = spawn( './srcds', ['-autoupdate', '-nohltv', '-maxplayers', '20', '+rcon_password', tf2_rcon, '+map', map, '-game', 'tf'], {cwd: '../tfds/orangebox'} );
+	tf2 = spawn( './srcds_run', ['-autoupdate', '-nohltv', '-maxplayers', '20', '-game', 'tf', '+rcon_password', tf2_rcon, '+map', map], {cwd: '../tfds/orangebox'} );
 	tf2.on( 'exit', function( code ) {
 		tf2 = null;
+		try { rcon.end(); } catch ( ex ) {}
+		rcon = null;
+		clients.broadcast({ 'tf': 'offline' });
 	} );
 	tf2.stdout.setEncoding( 'utf8' );
 	tf2.stdout.on( 'data', function( data ) {
 		buffer += data;
 		console.log(data);
+		if ( buffer.indexOf( 'Executing dedicated server config file' ) != -1 && !rcon ) {
+			rcon = new Sock();
+			rcon.connect( 27015 );
+			rcon.setNoDelay();
+			rcon.setKeepAlive( true );
+			rconSend( 2, tf2_rcon );
+			clients.broadcast({ 'tf': 'online' });
+		}
 	} );
 	tf2.stderr.setEncoding( 'utf8' );
 	tf2.stderr.on( 'data', function( data ) {
 		buffer += data;
 		console.error(data);
 	} );
-	rcon = new socket();
-	rcon.connect( 27015 );
-	rcon.setNoDelay();
-	rcon.setKeepAlive( true );
-	rconSend( 2, tf2_rcon );
+	rcon = null;
 }
 
-http.createServer( function( req, res ) {
-	var query = parse(req.url, true).query;
-	if ( query && 'start' in query && !tf2 ) {
-		spawnTF2( query.start );
-		res.writeHead( 302, {'Location': '/'} );
+var server = http.createServer( function( req, res ) {
+	fs.readFile( 'pubcomp.html', function( err, data ) {
+		if ( err ) { res.end(); return; }
+		res.writeHead( 200, {'Content-Type': 'text/html'} );
+		res.write( data, 'utf8' );
 		res.end();
-		return;
+	} );
+} );
+server.listen( 27014 );
+
+var socket = io.listen( server ), clients = [];
+clients.broadcast = function( msg ){
+	for ( var i = 0; i < clients.length; i++ ) {
+		clients[i].broadcast( msg );
 	}
-	if ( query && 'join' in query && tf2 ) {
-		if ( !/^STEAM_\d:\d:\d+$/.test( query.join ) ) {
-			res.writeHead( 302, {'Location': '/'} );
-			res.end();
+}
+socket.on( 'connection', function( client ) {
+	clients.push( client );
+
+	clients.broadcast({ 'numOnline': clients.length });
+	client.broadcast({ 'tf': tf2 ? ( rcon ? 'online' : 'starting' ) : 'offline' });
+
+	client.on('message', function(message){
+		if ( typeof message != 'object' ) {
 			return;
 		}
+		if ( 'action' in message ) {
+			switch ( message.action ) {
+				case 'tf':
+					client.broadcast({ 'tf': tf2 ? ( rcon ? 'online' : 'starting' ) : 'offline' });
+					break;
+				case 'create_match':
+					spawnTF2( 'mge' );
+					clients.broadcast({ 'tf': 'starting' });
+					break;
+				case 'join_match':
+					if ( /^STEAM_\d:\d:\d+$/.test( message.steamid ) ) {
+						rconSend( 3, 'pubcomp_add_steamid ' + message.steamid );
+						client.broadcast({ join: ip + ':27015' });
+					}
+					break;
+			}
+		}
+	});
 
-		rconSend( 3, 'pubcomp_add_steamid ' + query.join );
-		res.writeHead( 302, {'Location': 'steam://connect/' + ip} );
-		res.end();
-		return;
-	}
-	
-	res.writeHead( 200, {'Content-Type': 'text/html'} );
-	if ( tf2 ) {
-		res.end( '<pre>TF2 is running. <a href="?join=STEAM_0:0:26649930">Join the game!</a>\n\n' + buffer + '</pre>' );
-	} else {
-		res.end( '<pre>TF2 is NOT running. <a href="?start=mge">Start it.</a>\n\n' + buffer + '</pre>' );
-	}
-} ).listen( 27014, '0.0.0.0' );
+	client.on('disconnect', function(){
+		clients.splice(clients.indexOf(client), 1);
+		clients.broadcast({ 'numOnline': clients.length });
+	});
+} );
