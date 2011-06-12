85e8aaff2efd23ad5cf16644c04f9ff18bd548f1
Nightgunner5 (Ben L.) <nightgunner5@llamaslayers.net>
2011-06-12 17:41:19 -0500

    RCON works perfectly now

--- pubcomp.js
+++ pubcomp.js
@@ -3,6 +3,7 @@ var http = require('http'),
 	fs = require('fs'),
 	spawn = require('child_process').spawn,
 	dgram = require("dgram"),
+	bignumber = require('bignumber').BigInteger,
 	tf2 = null,
 	tf2_rcon = '',
 	rcon_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split( '' ),
@@ -14,7 +15,7 @@ var http = require('http'),
 require('./logsocket').create(function(line) {
 	console.log( '%s', line );
 	if ( line.indexOf( 'Started map' ) != -1 && !rcon ) {
-		rcon = require('./rcon').create( 27015, ip ).password( tf2_rcon );
+		rcon = require('./rcon').create( 27015, '127.0.0.1' ).password( tf2_rcon );
 		tf2state = 'online';
 		sendTF2State( socket );
 	}
@@ -68,12 +69,20 @@ socket.on( 'connection', function( client ) {
 						}, 100 );
 					} else if ( ( path = /^http:\/\/(?:www\.)?steamcommunity\.com(\/(profile|id)\/[^\/]+)$/.exec( message.steamid ) ) && path.length ) {
 						path = path[1];
-						http.get({ host: 'www.steamcommunity.com', port: 80, path: path + '?xml=1' }, function( res ) {
+						http.get( { host: 'steamcommunity.com', port: 80, path: path + '?xml=1' }, function( res ) {
+							// Once we find the person's steam ID, don't add all their friends to the permission list as well.
+							var found = false;
 							res.setEncoding( 'utf8' );
 							res.on( 'data', function( chunk ) {
+								// Wait for it...
+								if ( found )
+									return;
+								// I typed that without noticing what I had typed.
+								found = true;
 								var ID64 = /<steamID64>(\d+)<\/steamID64>/.exec( chunk );
 								if ( ID64 && ID64.length ) {
-									var steamID = 'STEAM_0:' + ( ID64[1] % 2 ) + ':' + ( ID64[1] - 76561197960265728 ) / 2;
+									ID64 = new bignumber( ID64[1] );
+									var steamID = 'STEAM_0:' + ID64.mod( new bignumber( '2' ) ) + ':' + ( ID64.subtract( new bignumber( '76561197960265728' ) ).shiftRight( 1 ) );
 									rcon.send( 'pubcomp_add_steamid ' + steamID );
 									client.broadcast({ 'joinserver': ip + ':27015' });
 								}
--- rcon.js
+++ rcon.js
@@ -17,31 +17,49 @@ function rconSend( rcon, status, command ) {
 
 function RCON( port, ip ) {
 	this.connection = createConnection( port, ip );
-	this.connection.setNoDelay();
-	this.connection.setKeepAlive( true );
+	//this.connection.setNoDelay();
+	//this.connection.setKeepAlive( true );
+	this.connected = false;
 
 	var self = this;
-	this.connection.on( 'connect', function(){ self.emit( 'connect' ); } );
+	this.connection.on( 'connect', function(){ self.connected = true; self.emit( 'connect' ); } );
 	this.connection.on( 'data', function( data ){ self.emit( 'data', data ); } );
 	this.connection.on( 'end', function(){ self.emit( 'end' ); } );
 	this.connection.on( 'timeout', function(){ self.emit( 'timeout' ); } );
 	this.connection.on( 'drain', function(){ self.emit( 'drain' ); } );
 	this.connection.on( 'error', function( exception ){ self.emit( 'error', exception ); } );
-	this.connection.on( 'close', function( had_error ){ self.emit( 'had_error', had_error ); } );
+	this.connection.on( 'close', function( had_error ){ self.emit( 'close', had_error ); } );
 
 	this.connection.on( 'data', function( data ) {
-		self.emit( 'response', data.slice( 16, data.length - 2 ).toString( 'utf8' ) );
+		if ( data.length >= 18 )
+			self.emit( 'response', data.slice( 16, data.length - 2 ).toString( 'utf8' ) );
 	} );
 }
 util.inherits(RCON, process.EventEmitter);
 
 RCON.prototype.password = function( passwd ) {
-	rconSend( this.connection, 3, passwd );
+	if ( this.connected ) {
+		//console.log( 'RCON(3) SEND: %s', passwd );
+		rconSend( this.connection, 3, passwd );
+	} else {
+		//console.log( 'RCON(3) DELAYED: %s', passwd );
+		this.on( 'connect', function() {
+			this.password( passwd );
+		} );
+	}
 	return this;
 };
 
 RCON.prototype.send = function( command ) {
-	rconSend( this.connection, 2, command );
+	if ( this.connected ) {
+		//console.log( 'RCON(2) SEND: %s', command );
+		rconSend( this.connection, 2, command );
+	} else {
+		//console.log( 'RCON(2) DELAYED: %s', command );
+		this.on( 'connect', function() {
+			this.send( command );
+		} );
+	}
 	return this;
 };
 
