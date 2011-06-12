7e9aef41b55190f43417f2450f25669de6244e75
Ben L <nightgunner5@llamaslayers.net>
2011-06-12 11:43:35 -0500

    Convert mapchooser and logsocket to Node modules

--- logsocket.js
+++ logsocket.js
@@ -1,13 +1,20 @@
 var dgram = require('dgram');
 
-var log = dgram.createSocket( 'udp4' );
-log.on( 'message', function( msg, rinfo ) {
-	if ( rinfo.address != '127.0.0.2' || rinfo.port != 27015 )
-		return;
-	if ( msg.slice( 0, 7 ).toString( 'base64' ) != '/////1JMIA==' )
-		console.log( '???: %s', msg );
-	console.log( '%s', msg.slice( 7 ).toString( 'utf8' ).replace( /\n/g, '' ) );
-} );
-log.bind( 57015, '127.0.0.2' ); // 127.0.0.1 doesn't work for some odd reason
-process.stdin.resume();
+// var logparser = require('tf2logparser').TF2LogParser.create();
+// function parseLine( line ){ logParser.parseLine( line ); }
+// require('logsocket').create( parseLine ).bind( 57015, '127.0.0.2' );
+exports.create = function( callback ) {
+	if ( typeof callback != 'function' )
+		callback = console.log;
 
+	var log = dgram.createSocket( 'udp4' );
+	log.on( 'message', function( msg, rinfo ) {
+		if ( msg.slice( 0, 7 ).toString( 'base64' ) != '/////1I=' ) {
+			// Not a log message
+			return;
+		}
+		var line = msg.slice( 7 ).toString( 'utf8' ).replace( /\n/g, '' );
+		callback( line );
+	} );
+	return log;
+}
