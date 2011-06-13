a8c1d28b4815f7e1bac20f13ce1b2e1f15450ae5
Nightgunner5 (Ben L.) <nightgunner5@llamaslayers.net>
2011-06-13 12:06:01 -0500

    Implement Schulze method

--- mapchooser.js
+++ mapchooser.js
@@ -54,8 +54,9 @@ function transformPlayed( played ) {
 }
 
 
-//	realPriorities = ['cp_badlands', 'cp_granary', 'cp_gravelpit', 'cp_freight_final1', ''], // All non-listed maps are ranked where the empty string is (tied)
-//	lastPlayed = ['cp_granary', 'cp_badlands', 'cp_freight_final1', 'cp_badlands']; // Only the first four are used.
+// realPriorities = ['cp_badlands', 'cp_granary', 'cp_gravelpit', 'cp_freight_final1', ''], // All non-listed maps are ranked where the empty string is (tied)
+// lastPlayed = ['cp_granary', 'cp_badlands', 'cp_freight_final1', 'cp_badlands']; // Only the first four are used.
+// Return: ['cp_gravelpit', ...] // Transformed map array with tied maps randomly ordered
 exports.transformPriorities = function( realPriorities, lastPlayed ) {
 	var priorities = realPriorities.slice( 0 ), unusedMaps = maps.slice( 0 ).filter( function( map ){ return priorities.indexOf( map ) == -1; } );
 	unusedMaps.shuffle();
@@ -68,3 +69,65 @@ exports.transformPriorities = function( realPriorities, lastPlayed ) {
 
 	return priorities;
 }
+
+// allPriorities is an array of arrays, one for each player, which contain the maps in order from most wanted to least wanted
+// Return: ['winner', 'second', 'third', ...]
+exports.computeWinner = function( allPriorities ) {
+	var mapScores = {};
+
+	maps.forEach( function( map1 ) {
+		mapScores[map1] = {};
+		maps.forEach( function( map2 ) {
+			mapScores[map1][map2] = 0;
+		} );
+	} );
+
+	function addMapScore( map, higherthan ) {
+		higherthan.forEach( function( otherMap ) {
+			mapScores[map][otherMap]++;
+		} );
+	}
+
+	allPriorities.forEach( function( priorities ) {
+		for ( var i = 0; i < priorities.length - 1; i++ ) {
+			addMapScore( priorities[i], priorities.slice( i + 1 ) );
+		}
+	} );
+
+	// Based on pseudocode from http://en.wikipedia.org/wiki/Schulze_method
+
+	var transformedMapScores = {};
+	maps.forEach( function( map1 ) {
+		transformedMapScores[map1] = {};
+		maps.forEach( function( map2 ) {
+			if ( map1 != map2 ) {
+				if ( mapScores[map1][map2] > mapScores[map2][map1] ) then
+					transformedMapScores[map1][map2] = mapScores[map1][map2];
+				} else {
+					transformedMapScores[map1][map2] = 0;
+				}
+			}
+		} );
+	} );
+
+    maps.forEach( function( map1 ) {
+    	maps.forEach( function( map2 ) {
+    		if ( map1 != map2 ) {
+    			maps.forEach( function( map3 ) {
+    				if ( map1 != map3 && map2 != map3 ) {
+    					transformedMapScores[map2][map3] = Math.max( transformedMapScores[map2][map3],
+    							Math.min( transformedMapScores[map2][map1], transformedMapScores[map1][map3] ) );
+    				}
+    			} );
+    		}
+    	} );
+    } );
+
+	return maps.slice( 0 ).sort( function( a, b ){
+		if ( transformedMapsScores[a][b] == transformedMapScores[b][a] )
+			return 0;
+		if ( transformedMapsScores[a][b] > transformedMapScores[b][a] )
+			return -1;
+		return 1;
+	} );
+};
